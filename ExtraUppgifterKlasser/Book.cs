a06883d502e257642ede9ec8475c6331087f1a84
Jontiq <99jonathancar@gmail.com>
2025-10-01 16:35:35 +0200

    Add project files.

---
+++ ExtraUppgifterKlasser/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraUppgifterKlasser
+{
+    internal class Book
+    {
+        //Props
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public bool IsBorrowed { get; set; }
+
+        //Konstruktor
+        public Book (string Title, string Author, string ISBN, bool IsBorrowed)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.ISBN = ISBN;
+            this.IsBorrowed = IsBorrowed;
+        }
+
+        //Methods
+
+        public void Borrow()
+        {
+            if (IsBorrowed == false)
+            {
+                IsBorrowed = true;
+                Console.WriteLine($"\"{Title}\" has now been borrowed.");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, \"{Title}\" is already borrowed.");
+            }
+        }
+    }
+}
---
+++ ExtraUppgifterKlasser/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ExtraUppgifterKlasser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Title", "Author", "928374", false);
+
+            book1.Borrow();
+            book1.Borrow();
+        }
+    }
+}
